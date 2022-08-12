279427fa3295930143092f1a39ca7354f5bd3aed
Tien Phuc <leesinvn123@gmail.com>
2022-08-12 21:17:57 +0700

    [Teko-interview]: Test UI Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import {} from "antd";
+import { Table, Typography } from "antd";
 import "antd/dist/antd.css";
 import { CheckCircleFilled } from "@ant-design/icons";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Typography.Link>
+        <CheckCircleFilled />
+      </Typography.Link>
+
+      <Table bordered />
+    </div>
+  );
 }
 
 export default App;
